e32aaef98041052b4899751a3b020126e7661366
Ben Mojo <155706745+oakes777@users.noreply.github.com>
2025-04-15 14:50:28 -0700

    sdev334 wk3 tues binary hex aRGB class

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,15 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Yay bitwise operations!");
+        int number = 0b101010111011001111011011;
+        //right shift operator
+        number = number >> 8;
+        //I've now right shifted my binary #s by 8 digit spaces
+        //now my 8 least significant spaces represent GREEN in rgb 24 bit
+        //(if I want red I can: number = number >> 16;)
+        
+        //but I'm only interested in the 'xxxxXXXX' digit spaces
+        int mask = 0xff;
+        int result = number & mask;
+        System.out.println(Integer.toBinaryString(result));
     }
 }
