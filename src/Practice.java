f3d693ef92f9a2c8d3719cb411e3ef328c21bfa2
dannymccarragher <dannymccarragher@gmail.com>
2025-04-15 14:48:53 -0700

    Live Code 4/15

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,23 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Yay bitwise operations!");
+        // 0b represent binary
+        //0x represents hexadecimal
+        int number = 0b01000110010; // 0x1f
+
+        number = number >> 8;
+
+        int mask = 0xff;
+
+        int result = number & mask;
+
+        // System.out.println(number);
+
+        System.out.println(Integer.toBinaryString(result));
+
+        // System.out.println(Integer.toBinaryString(number));
+
+        // System.out.println("Yay bitwise operations!");
+
+        
     }
 }
