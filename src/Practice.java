ae5336ae6ace206a26cae31e1dfc871666fca33f
TavSingh <tavparsadsingh2@gmail.com>
2025-04-15 14:49:25 -0700

    worked in class

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,9 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Yay bitwise operations!");
+        int num = 0b110100101100111001011011;
+        num = num >> 16;
+        int mask = 0xff;
+        int result = num & mask;
+        System.out.println(Integer.toBinaryString(result));
     }
 }
