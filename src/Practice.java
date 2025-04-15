3dfa50d59cbbac3793ff3b18ef9aafe45ef82879
Z1springer <zdspringer99@gmail.com>
2025-04-15 14:49:37 -0700

    Finished in-class live code

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,16 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Yay bitwise operations!");
+        // Starting with "0b" means that you are typing the number in binary
+        // int number = 0b1110;
+        // Starting with "0x" means that you are typing the number in hexadecimal
+        // int number = 0b00011111; // 0x1f; = 31
+        // System.out.println(Integer.toBinaryString(number));
+        int number = 0b110100101100111001011011;
+        number = number >> 8; // Shift over 8 times
+        System.out.println(Integer.toBinaryString(number));
+        // int mask = 0b00001111; // same as 0b1111 and 0x0f and 0xf
+        int mask = 0xff;
+        int result = number & mask;
+        System.out.println(Integer.toBinaryString(result));
     }
 }
