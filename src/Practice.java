512b22c03edfcba416e5d2d69008ff8d8aecc3ad
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-04-15 14:50:21 -0700

    in-class bitwise livecode

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,9 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Yay bitwise operations!");
+        int number = 0b110011100101101101011011;
+        number = number >> 8;
+        int mask = 0xff;
+        int result = number & mask;
+        System.out.println(Integer.toBinaryString(result));
     }
 }
