94ce36421728f28e9322d52c719a4a128c74e986
Jameson Farmer <thejamesonfarmer@gmail.com>
2025-04-16 20:50:28 -0700

    Completed!

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,9 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Yay bitwise operations!");
+        int number = 0b11010010110011100100101011;
+        number = number >> 16;
+        int mask = 0xff;
+        int result = number & mask;
+        System.out.println(Integer.toBinaryString(result));
     }
 }
